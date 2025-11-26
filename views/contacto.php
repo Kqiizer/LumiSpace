32f26bcea490cb86a69111dfce5fe5b52f85776a
xim8989 <xcuevas@ucol.mx>
2025-11-25 21:28:05 -0600

    fernando imagen

--- views/contacto.php
+++ views/contacto.php
@@ -227,9 +227,8 @@ $BASE = defined('BASE_URL') ? rtrim(BASE_URL, '/') . '/' : '/';
 
                 <div class="team-card">
                     <div class="team-image">
-                        <div class="image-placeholder">
-                            <i class="fas fa-user"></i>
-                        </div>
+                        <img src="<?= $BASE ?>images/imagenes-integrantes/fernando-arroyo.jpeg" 
+                             alt="Fernando Arroyo">
                         <div class="team-social">
                             <a href="#"><i class="fab fa-linkedin"></i></a>
                             <a href="#"><i class="fab fa-twitter"></i></a>
